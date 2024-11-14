83d4bf27e461e1448054c758e3daf752ce6470f3
kiran_kumar <86544378+Chennakiran2004@users.noreply.github.com>
2024-11-14 11:56:52 +0530

    Add files via upload

---
+++ wallefy_backend/models.py
@@ -0,0 +1,127 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from .Enums import *
+
+
+# User Model with Custom Fields
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                               editable=False)
+    username = models.CharField(max_length=50, null=True, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=50)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='wallefy_backend_user_groups',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='wallefy_backend_user_permissions',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+# UserRole Model
+class UserRole(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10,
+                            choices=RoleChoices.list_of_values())
+
+    def __str__(self):
+        return f"{self.user} - {self.role}"
+
+
+# UserProfile Model
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=10,
+                              choices=GenderChoices.list_of_values())
+
+    def __str__(self):
+        return f"{self.user}'s Profile"
+
+
+class UserPreferenceDetails(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    month = models.DateField(auto_now=True)
+    preference = models.CharField(max_length=150,
+                                  choices=PreferenceChoices.list_of_values())
+    location = models.CharField(max_length=150,
+                                choices=AreaEnum.list_of_values())
+    city = models.CharField(max_length=150,
+                            choices=LocationChoices.list_of_values(),
+                            default='hyderabad')
+    account_balance = models.DecimalField(max_digits=10, decimal_places=2,
+                                          default=0.00)
+
+    def __str__(self):
+        return f"{self.user} - {self.location}"
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=150,
+                            choices=LocationChoices.list_of_values(),
+                            default="Hyderabad")
+    area = models.CharField(max_length=50, choices=AreaEnum.list_of_values(),
+                            unique=True)
+
+    def __str__(self):
+        return self.area
+
+
+class LocationWiseCategoryDetails(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    Rent_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    Food_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    Shopping_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    Travelling_percentage = models.DecimalField(max_digits=10,
+                                                decimal_places=2)
+    Health_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    Entertainment_percentage = models.DecimalField(max_digits=10,
+                                                   decimal_places=2)
+    Savings_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    Miscellaneous_percentage = models.DecimalField(max_digits=10,
+                                                   decimal_places=2)
+
+    gender = models.CharField(max_length=10,
+                              choices=GenderChoices.list_of_values())
+    preference = models.CharField(max_length=150,
+                                  choices=PreferenceChoices.list_of_values())
+
+    def __str__(self):
+        return f"{self.location} - {self.preference}"
+
+
+class UserExpense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50,
+                                choices=Category.list_of_values())
+    description = models.TextField(blank=True, null=True)
+    expenses_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.category}: {self.expenses_amount}"
+
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Feedback(models.Model):
+    user_feedback = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating_stars = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    description = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user_feedback} - {self.description}"
---
+++ walletfy/urls.py
@@ -0,0 +1,57 @@
+"""
+URL configuration for walletfy project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from wallefy_backend.user_views import get_login_interactor_view, \
+    get_signup_interactor_view, get_update_password_view, \
+    update_user_profile_view, get_user_profile_api_view, \
+    refresh_access_token_view, user_logout_view
+from wallefy_backend.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user_account/login/v1', get_login_interactor_view),
+    path('user_account/signup/v1', get_signup_interactor_view),
+    path('user_account/update_password/v1', get_update_password_view),
+    path('get/user_profile/v1', get_user_profile_api_view),
+    path('update/user_profile/v1', update_user_profile_view),
+    path("get/refresh_access_token/v1", refresh_access_token_view),
+    path("user_account/logout/v1", user_logout_view),
+    path('get_user_details/', get_user_details, name='get_user_details'),
+    path('update_user_expense', update_user_expense,
+         name='update_user_expense'),
+    path('feedback/', get_feedback),
+    path('store_user_data/', store_user_data),
+    path('get_last_five_transactions/', get_last_five_transactions),
+    path('get_last_all_transactions/', get_last_all_transactions),
+    path('get_transaction_filters/', get_transaction_filters),
+    path('get_user_pie_chart_financial_transactions/',
+         get_user_pie_chart_financial_transactions),
+    path('get_user_income_pie_chart/', get_user_income_pie_chart),
+    path('delete_user_expense/', delete_user_expense),
+    path('get_transaction_details/', get_transaction_details),
+    path('get_user_expense_suggestions/', get_user_expense_suggestions),
+    path('get_user_expenses_comparison_at_eom/',
+         get_user_expenses_comparison_at_eom),
+    path('accounts/', include('allauth.urls')),
+
+    path('generate_personalized_response/',
+         generate_personalized_response,
+         name='generate_personalized_response'),
+
+]
